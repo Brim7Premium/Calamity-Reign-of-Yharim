9b0be5e45967782fab5d471e50a4b2fe6ef3e7ec
Brim7Premium <121100311+Brim7Premium@users.noreply.github.com>
2023-03-05 15:10:07 -0500

    Removed A*

--- Reign of Yharim/Assets/Scripts/ParallaxBackground.cs
+++ Reign of Yharim/Assets/Scripts/ParallaxBackground.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
     [SerializeField] private Vector2 parallaxEffectMultiplier;
+    [SerializeField] private bool infiniteLooping;
 
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
     private float textureUnitSizeX;
+    private float textureOffsetX;
+
     private void Start()
     {
         cameraTransform = Camera.main.transform;
@@ -16,11 +19,27 @@ public class ParallaxBackground : MonoBehaviour
         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
         Texture2D texture = sprite.texture;
         textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+
+        if (infiniteLooping)
+        {
+            textureOffsetX = transform.position.x;
+        }
     }
+
     private void LateUpdate()
     {
         Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
         transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
         lastCameraPosition = cameraTransform.position;
+
+        if (infiniteLooping)
+        {
+            if (Mathf.Abs(cameraTransform.position.x - textureOffsetX) >= textureUnitSizeX)
+            {
+                float offsetPositionX = (cameraTransform.position.x - textureOffsetX) % textureUnitSizeX;
+                textureOffsetX = cameraTransform.position.x - offsetPositionX;
+                transform.position = new Vector3(textureOffsetX, transform.position.y, transform.position.z);
+            }
+        }
     }
 }
