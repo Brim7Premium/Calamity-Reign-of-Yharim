f24cf0c70316f73e3f9ee4f5bc9dcfc72998791d
Brim7Premium <121100311+Brim7Premium@users.noreply.github.com>
2023-03-06 07:19:24 -0500

    Working on loop effect

--- Reign of Yharim/Assets/Scripts/ParallaxBackground.cs
+++ Reign of Yharim/Assets/Scripts/ParallaxBackground.cs
@@ -1,45 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    [SerializeField] private Vector2 parallaxEffectMultiplier;
-    [SerializeField] private bool infiniteLooping;
+    [SerializeField] private Transform[] backgrounds;
+    [SerializeField] private float[] parallaxScales;
+    [SerializeField] private float smoothing = 1f;
 
     private Transform cameraTransform;
-    private Vector3 lastCameraPosition;
-    private float textureUnitSizeX;
-    private float textureOffsetX;
+    private Vector3 previousCameraPosition;
 
     private void Start()
     {
         cameraTransform = Camera.main.transform;
-        lastCameraPosition = cameraTransform.position;
-        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-        Texture2D texture = sprite.texture;
-        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
-
-        if (infiniteLooping)
-        {
-            textureOffsetX = transform.position.x;
-        }
+        previousCameraPosition = cameraTransform.position;
     }
 
     private void LateUpdate()
     {
-        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
-        lastCameraPosition = cameraTransform.position;
-
-        if (infiniteLooping)
+        for (int i = 0; i < backgrounds.Length; i++)
         {
-            if (Mathf.Abs(cameraTransform.position.x - textureOffsetX) >= textureUnitSizeX)
+            float parallax = (previousCameraPosition.x - cameraTransform.position.x) * parallaxScales[i];
+            float backgroundTargetPosX = backgrounds[i].position.x - parallax;
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+
+            if (backgrounds[i].position.x < cameraTransform.position.x - (backgrounds[i].GetComponent<SpriteRenderer>().bounds.size.x / 2))
             {
-                float offsetPositionX = (cameraTransform.position.x - textureOffsetX) % textureUnitSizeX;
-                textureOffsetX = cameraTransform.position.x - offsetPositionX;
-                transform.position = new Vector3(textureOffsetX, transform.position.y, transform.position.z);
+                float offsetPositionX = (backgrounds[i].GetComponent<SpriteRenderer>().bounds.size.x - 0.01f);
+                backgrounds[i].position = new Vector3(backgrounds[i].position.x + offsetPositionX, backgrounds[i].position.y, backgrounds[i].position.z);
+            }
+            else if (backgrounds[i].position.x > cameraTransform.position.x + (backgrounds[i].GetComponent<SpriteRenderer>().bounds.size.x / 2))
+            {
+                float offsetPositionX = (backgrounds[i].GetComponent<SpriteRenderer>().bounds.size.x - 0.01f);
+                backgrounds[i].position = new Vector3(backgrounds[i].position.x - offsetPositionX, backgrounds[i].position.y, backgrounds[i].position.z);
             }
         }
+
+        previousCameraPosition = cameraTransform.position;
     }
 }
