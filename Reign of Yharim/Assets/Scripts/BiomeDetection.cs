a01f698138714ce1af9a9023ebd7c03641d4a340
Brim7Premium <121100311+Brim7Premium@users.noreply.github.com>
2023-03-16 14:39:41 -0400

    12x12 Biome Air
    Changed the biome air to use a new 12x12 texture

--- Reign of Yharim/Assets/Scripts/BiomeDetection.cs
+++ Reign of Yharim/Assets/Scripts/BiomeDetection.cs
@@ -45,57 +45,121 @@ public class BiomeDetection : MonoBehaviour
                 currentTileName = tileSpriteName; //set the current tile name to the name of the sprite
                 if (tileSpriteName == "Astral") //if the tile's name is astral
                 {
+                    /*
+                    Astral Infection
                     mainCam.backgroundColor = new Color(0.06666667f, 0.003921569f, 0.07450981f);
-                    //audioSource.clip = Astral;
-                    //audioSource.Play();
+                    audioSource.clip = Astral;
+                    audioSource.Play(); 
+                    */
                 }
                 if (tileSpriteName == "Desert")
                 {
+                    /*
+                    Desert
                     mainCam.backgroundColor = new Color(1f, 0.9850028f, 0.8264151f);
-                    //audioSource.clip = Desert;
-                    //audioSource.Play();
+                    audioSource.clip = Desert;
+                    audioSource.Play(); 
+                    */
                 }
                 if (tileSpriteName == "Blight")
                 {
+                    /*
+                    Blight
                     mainCam.backgroundColor = new Color(0.09783139f, 0.1509434f, 0.06778213f);
-                    //audioSource.clip = Blight;
-                    //audioSource.Play();
+                    audioSource.clip = Blight;
+                    audioSource.Play();
+                    */
                 }
                 if (tileSpriteName == "Bloody")
                 {
+                    /*
+                    Bloody Meteor
                     mainCam.backgroundColor = new Color(0.09433961f, 0.005603133f, 0f);
-                    //audioSource.clip = Bloody;
-                    //audioSource.Play();
+                    audioSource.clip = Bloody;
+                    audioSource.Play();
+                    */
                 }
                 if (tileSpriteName == "Ocean")
                 {
+                    /*
+                    Ocean
                     mainCam.backgroundColor = new Color(0.345283f, 0.8541663f, 1f);
-                    //audioSource.clip = Ocean;
-                    //audioSource.Play();
+                    audioSource.clip = Ocean;
+                    audioSource.Play();
+                    */
                 }
                 if (tileSpriteName == "Sulfur")
                 {
+                    /*
+                    Sulphurous Sea
                     mainCam.backgroundColor = new Color(0.5457409f, 0.8962264f, 0.3179068f);
-                    //audioSource.clip = Sulfur;
-                    //audioSource.Play();
+                    audioSource.clip = Sulfur;
+                    audioSource.Play();
+                    */
                 }
                 if (tileSpriteName == "Tundra")
                 {
+                    /*
+                    Tundra
                     mainCam.backgroundColor = new Color(0.7415094f, 1f, 0.95700063f);
-                    //audioSource.clip = Tundra;
-                    //audioSource.Play();
+                    audioSource.clip = Tundra;
+                    audioSource.Play();
+                    */
                 }
                 if (tileSpriteName == "Forest")
                 {
-                    mainCam.backgroundColor = new Color(0.5622642f, 0.9691256f, 1f);
+                    //Spawn plains/Forest
+                    //mainCam.backgroundColor = new Color(0.5622642f, 0.9691256f, 1f);
                 }
                 if (tileSpriteName == "Light")
                 {
-                    mainCam.backgroundColor = new Color(1f, 1f, 1f);
+                    //Light
+                    //mainCam.backgroundColor = new Color(1f, 1f, 1f);
                 }
                 if (tileSpriteName == "Jungle")
                 {
-                    mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f);
+                    //Jungle
+                    //mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f);
+                }
+                if (tileSpriteName == "Sky")
+                {
+                    //Sky
+                    //mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f); Change this later
+                }
+                if (tileSpriteName == "Underworld")
+                {
+                    //Underworld
+                    //mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f); Change this later
+                }
+                if (tileSpriteName == "Space")
+                {
+                    //Space
+                    //mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f); Change this later
+                }
+                if (tileSpriteName == "Crags")
+                {
+                    //Brimstone Crags/Azafure
+                    //mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f); Change this later
+                }
+                if (tileSpriteName == "Abyss2")
+                {
+                    //Abyss tier 1
+                    //mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f); Change this later
+                }
+                if (tileSpriteName == "SunkenSea")
+                {
+                    //Sunken Sea
+                    //mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f); Change this later
+                }
+                if (tileSpriteName == "Obsidian")
+                {
+                    //Obsidian Cliffs
+                    //mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f); Change this later
+                }
+                if (tileSpriteName == "Garden")
+                {
+                    //Profaned Garden
+                    //mainCam.backgroundColor = new Color(0.5254902f, 1f, 0.8364275f); Change this later
                 }
             }
         }
