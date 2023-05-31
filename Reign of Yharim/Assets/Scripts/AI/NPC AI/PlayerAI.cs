2fcff6bb51e41b77c8eac2fa46ab868a1ed77e44
Brim7Premium <121100311+Brim7Premium@users.noreply.github.com>
2023-05-31 16:36:50 -0400

    Animations

--- Reign of Yharim/Assets/Scripts/AI/NPC AI/PlayerAI.cs
+++ Reign of Yharim/Assets/Scripts/AI/NPC AI/PlayerAI.cs
@@ -10,49 +10,62 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 16f;
+    [SerializeField] public static bool isGrounded = false;
 
+    private string currentAnimationState;
     public Animator playerAnimator;
 
-    [SerializeField] public static bool isGrounded = false;
+    //constants can't be changed
+    const string PlayerIdle = "Player_idle";
+    const string PlayerWalk = "Player_walk";
+
     public override void SetDefaults()
     {
         lifeMax = 100;
         life = lifeMax;
         healthBar.SetMaxHealth(lifeMax);
+
+        playerAnimator = GetComponent<Animator>();
     }
     public override void AI()
     {
         Physics2D.IgnoreLayerCollision(10, 3);
 
-        //sets horizontal to -1 or 1 based on the player's input
-        horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxis("Horizontal"); //sets horizontal to -1 or 1 based on the player's input
+
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
+
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
-        if (Input.GetMouseButtonDown(0))
-        {
-            playerAnimator.SetBool("Attacking", true);
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            playerAnimator.SetBool("Attacking", false);
-        }
-        //sets the speed of the player along the x coordinate to 1 * speed or -1 * speed, allowing the player to move horizontally based on input
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
-        if (horizontal > 0 || horizontal < 0)
-            playerAnimator.SetBool("Moving", true);
-        else
-            playerAnimator.SetBool("Moving", false);
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y); //sets the speed of the player along the x coordinate to 1 * speed or -1 * speed, allowing the player to move horizontally based on input
+
         if (horizontal > 0) //right
             gameObject.transform.localScale = new Vector3(1, 1, 1);
-        else
+        else if (horizontal < 0) //left
             gameObject.transform.localScale = new Vector3(-1, 1, 1);
+
+        if (isGrounded)
+        {
+            if (horizontal != 0)
+                ChangeAnimationState(PlayerWalk);
+            else
+                ChangeAnimationState(PlayerIdle);
+        }
+    }
+
+    void ChangeAnimationState(string newAnimationState)
+    {
+        if (currentAnimationState == newAnimationState) return; //if currentAnimationState equals newAnimationState, stop the method (prevents animations from interupting themselves)
+
+        playerAnimator.Play(newAnimationState); //play the newState animation
+
+        currentAnimationState = newAnimationState; //set currentAnimationState to newAnimationState
     }
     private bool IsGrounded()
     {
--- Reign of Yharim/Assets/Scripts/CollisionSystem.cs
+++ Reign of Yharim/Assets/Scripts/CollisionSystem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CollisionSystem : MonoBehaviour
 {
     public Color color;
+    public float damage = 5f;
     private void OnTriggerStay2D(Collider2D collision)
     {
         Debug.Log("I'm hitting something!");
@@ -15,7 +16,7 @@ public class CollisionSystem : MonoBehaviour
                 Debug.Log("I'm hitting a player hitbox!");
                 color = new Color(1f, 0f, 0f, 0.1764706f);
                 collision.gameObject.GetComponent<SpriteRenderer>().color = color;
-                collision.gameObject.transform.parent.parent.SendMessage("TakeDamage", 5f); //damage the player for 5 damage. the five damage is temporary and will be replaced to work with custom damage for each entity
+                collision.gameObject.transform.parent.parent.SendMessage("TakeDamage", damage); //damage the player for 5 damage. the five damage is temporary and will be replaced to work with custom damage for each entity
             }
         }
         else
