3febb700135b356af5b3794f421e765ad27850c5
psychoFungus <ppk2506@mail.ru>
2023-11-04 17:17:06 +0300

    Better character controller

--- Reign of Yharim/Assets/Scripts/EnemySpawner.cs
+++ Reign of Yharim/Assets/Scripts/EnemySpawner.cs
@@ -13,24 +13,34 @@ public class EnemySpawner : MonoBehaviour
         {
             yield return new WaitForSeconds(regularSlimeSpawnRate);
 
-            Vector2 SpawnPosition = new Vector2(Random.Range(player.transform.position.x - 24f, player.transform.position.x + 24f), Random.Range(player.transform.position.y - 12f, player.transform.position.y + 12f));          
+            bool isPositionChoosen = false;
 
-            //I can't make enemies not to spawn in blocks in general, but this will reduce chances of this happening by a lot
-            while (!(Physics2D.Raycast(SpawnPosition, Vector2.down, 1, 1<<8) && !Physics2D.Raycast(SpawnPosition, Vector2.down, 0, 1<<8)))
+            Vector2 spawnPosition = new Vector2(Random.Range(player.transform.position.x - 24f, player.transform.position.x + 24f), 
+                                                Random.Range(player.transform.position.y - 12f, player.transform.position.y + 12f));
+
+            while (!isPositionChoosen)
             {
+                
                 for(int i = 0; i<10; i++)//The game will not stop working when there is no space to spawn an enemy
                 {
-                    if(!(Physics2D.Raycast(SpawnPosition, Vector2.down, 1, 1<<8) && !Physics2D.Raycast(SpawnPosition, Vector2.down, 0, 1<<8)))
-                    {
-                        SpawnPosition = new Vector2(Random.Range(player.transform.position.x - 24f, player.transform.position.x + 24f), Random.Range(player.transform.position.y - 12f, player.transform.position.y + 12f));
-                    }
-                    else break;
+                    spawnPosition = new Vector2(Random.Range(player.transform.position.x - 24f, player.transform.position.x + 24f), 
+                                                Random.Range(player.transform.position.y - 12f, player.transform.position.y + 12f));
+
+                    Vector2 distanceToPlayer = (Vector2)player.transform.position - spawnPosition;
+
+                    if(Physics2D.RaycastAll(spawnPosition, distanceToPlayer.normalized, distanceToPlayer.magnitude, 1<<8).Length % 2 == 1) continue;
+
+                    spawnPosition = Physics2D.Raycast(spawnPosition, Vector2.down, Mathf.Infinity, 1<<8).point;
+
+                    isPositionChoosen = true;
+
+                    break;
                 }
 
                 yield return new WaitForFixedUpdate();
             }
 
-            GameObject newEnemy = Instantiate(enemyToSpawn, SpawnPosition, Quaternion.identity);
+            GameObject newEnemy = Instantiate(enemyToSpawn, spawnPosition + Vector2.one, Quaternion.identity);
 
             newEnemy.SetActive(true);
         }
