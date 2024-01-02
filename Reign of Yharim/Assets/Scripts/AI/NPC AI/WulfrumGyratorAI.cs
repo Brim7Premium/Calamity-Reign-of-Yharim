fa5225f046a37ad2ffe21dcdfec05d65d82b882f
Acsazf-Play <143752165+Acsazf-Play@users.noreply.github.com>
2024-01-02 15:45:15 +0300

    Wulfgang
    - Added Wulfrum Enemies: Amplifier, Gyrator, Rover, Drone, Hovercraft.
    - Changes in GreenSlime's AI.

--- Reign of Yharim/Assets/Scripts/AI/NPC AI/WulfrumGyratorAI.cs
+++ Reign of Yharim/Assets/Scripts/AI/NPC AI/WulfrumGyratorAI.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class WulfrumGyratorAI : NPC
 {
-    private bool isGrounded = false;
-    private int curTargetPos;
     private bool spotted;
     private Color color;
+    private float AFKtimer = 0;
+    private float JumpCooldown = 0;
 
     const string GyratorSpin = "WulfrumGyrator_spin";
     const string ChargeGyratorSpin = "WulfrumGyrator_chargespin";
@@ -17,77 +17,48 @@ public class WulfrumGyratorAI : NPC
 
         NPCName = "WulfrumGyrator";
         Damage = 15;
-        LifeMax = 28;
+        LifeMax = 18;
         Life = LifeMax;
         target = GameObject.Find("Player");
 
         spotted = false;
+        IsWulfrumGuy = true;
 
     }
     public override void AI()
     {
-        UpdateVelocity();
+        animator.speed = (Mathf.Abs(rb.velocity.x) / 5f); // animate faster based on NPCs speed
+
         if (target != null)
         {
-            animator.speed = 0.8f;
-            ChangeAnimationState(GyratorSpin);
-
-            ai[0]++;//increment ai[0] by 1 every frame.(the framerate is capped at 60)
-            velocity *= 0.97f;//this is for smoothing the movement.
-
-            if (Vector2.Distance(transform.position, target.transform.position) > 20f && spotted == false) //if the player isn't close and the bool spotted equals false
+            if (ai[1] > 0)
             {
-                color = Color.green; //set color variable to green
-
-                if (ai[0] == 70.0f) //if it has been 70 frames, jump.
-                    velocity = new Vector2(velocity.x, 0.5f); //jump up vertically
-
-                if (ai[0] > 100.0f && isGrounded)
+                ChangeAnimationState(ChargeGyratorSpin);
+                ai[1] -= Time.deltaTime;
+                if (JumpCooldown > 0) JumpCooldown -= Time.deltaTime;
+                else if (IsGrounded && (int)(target.transform.position.y - 1.5f) > (int)(transform.position.y)) // Able to jump if charged
                 {
-                    velocity = Vector2.zero; //stop the slime from moving
-                    ai[0] = 0.0f;//reset ai[0] so we can jump again.
+                    rb.velocity += new Vector2(0, 18);
+                    JumpCooldown = 3;
                 }
             }
-            else
-            {
-                if (isGrounded)
-                {
-                    ai[0] = 0.0f;
-                    spotted = true;
-                    color = Color.red;
-                    if (ai[0] == 0.0f)
-                    {
-                        velocity = new Vector2(DirectionTo(transform.position, target.transform.position).x * 0.12f, velocity.y); //speed of 
-                    }
+            else ChangeAnimationState(GyratorSpin);
 
-                    /*if (//placeholder)
-                    {
-                        ai[0]++;
-                        velocity *= 0.97f;
-                        if (ai[0] == 1.0f) 
-                        {
-                            curTargetPos = GetTargetDirectionX();
-                            velocity.x = curTargetPos * 0.2f; //jump towards the player
-                            velocity.y = 0.2f; //at the same time jump up
-                        }
-                        if (ai[0] > 1.0f && !isGrounded)
-                        {
-                            velocity.x = curTargetPos * 0.1f; //continue going horizontally
-                        }
-                        if (ai[0] > 100.0f && isGrounded) //if ai[0] is greater than 100.0f (if this wasnt here, the slime would constantly reset when touching ground) and if the slime is grounded
-                        {
-                            velocity = Vector2.zero; //stop the slime from moving
-                            ai[0] = 0.0f;//reset ai[0] so we can jump again.
-                        }
-                    }*/
-                }
-
-            }
+            rb.velocity += (new Vector2(TargetDirection * 10f + (Mathf.Abs((transform.position.x - target.transform.position.x) * 0.4f)) * TargetDirection, 0) * Time.deltaTime); // Movement
 
             if (!IsVisibleFromCamera())
             {
                 spotted = false;
                 ai[0] = 0.0f;
+            }
+
+            if ((int)transform.position.x == (int)oldPosition.x && (int)transform.position.y == (int)oldPosition.y) AFKtimer += Time.deltaTime; //Helping Gyrator overcome slopes
+            else AFKtimer = 0;
+
+            if (AFKtimer >= 3)
+            {
+                rb.velocity += new Vector2(TargetDirection * 6f, 0);
+                AFKtimer = 0;
             }
 
             DrawDistance(transform.position, target.transform.position, color);
@@ -97,25 +68,5 @@ public class WulfrumGyratorAI : NPC
                 Destroy(gameObject); //despawn the object
             }
         }
-    }
-    private void FixedUpdate()
-    {
-        float extraHeight = 0.4f; //new float extraHeight equals 0.1
-        Color rayColor; //new color variable rayColor 
-
-        RaycastHit2D hit = Physics2D.Raycast(c2d.bounds.center, Vector2.down, c2d.bounds.extents.y + extraHeight, groundLayer); //new raycast2d called hit that starts from the center of the player rigidbody, goes down, and goes the extent of the rigidbody downwards + extraHeight. it only collides with the groundlayer variable
-
-        //Debug.Log(hit.collider);
-        if (hit.collider != null) //if the raycast is hitting something;
-        {
-            isGrounded = true; //isgrounded is true
-            rayColor = Color.green; //the raycolor is green
-        }
-        else
-        {
-            isGrounded = false; //isgrounded is false
-            rayColor = Color.red; //the raycolor is red
-        }
-        Debug.DrawRay(c2d.bounds.center, Vector2.down * (c2d.bounds.extents.y + extraHeight), rayColor); //draw the ray 
     }
 }
