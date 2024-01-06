e46821da4acb6c3c32771c4b224f7d0f371d2bd0
Brim7Premium <121100311+Brim7Premium@users.noreply.github.com>
2024-01-06 12:24:21 -0500

    Changed swiming
    gonna port this to the merged branch

--- Reign of Yharim/Assets/Scripts/Player/PlayerAI.cs
+++ Reign of Yharim/Assets/Scripts/Player/PlayerAI.cs
@@ -15,7 +15,9 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 	[SerializeField] private float acceleration;
 	[SerializeField] private float deacceleration;
 	[SerializeField] private float velPower;
+	private Vector2 axis;
 	private float xAxis;
+	private float yAxis;
 	public float isFacing;
 
 	[Header("Jumping")]
@@ -35,9 +37,8 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 	[Header("Misc")]
 	[SerializeField] private GameObject targetTestObject;
 	public GameObject worldManager;
-	public Rigidbody2D rigidbodycomp;
 
-	[SerializeField] [Range(1, 180)]private int framerate; //create int with range of 1 to 180, used for setting framerate. Why this is in the player's AI  script will remain unknown for eternity
+	[SerializeField] [Range(1, 180)] private int framerate; //create int with range of 1 to 180, used for setting framerate. Why this is in the player's AI  script will remain unknown for eternity
 
 	//constants can't be changed
 	const string PlayerIdle = "Player_idle";
@@ -54,18 +55,20 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 		NPCName = "Player";
 		Damage = 0; //Note to future developers/self, this can be used for times when the player does deal contact damage to enemies. armor sets are an example. right now, it's useless.
 		LifeMax = 100;
-		Life = LifeMax;        
+		Life = LifeMax;
 
 		rb.velocity = new Vector2(rb.velocity.x, Vector2.zero.y);
 	}
 
 	public override void AI() //every frame (Update)
 	{
-		xAxis = Input.GetAxis("Horizontal"); //sets horizontal to -1 or 1 based on the player's input
+		//xAxis = Input.GetAxis("Horizontal"); //sets xaxis to -1 or 1 based on the player's input
+		//yAxis = Input.GetAxis("Vertical");
+		axis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
 		if (Input.GetButtonDown("Jump")) //if the jump button is being pressed...
 		{
-			if (isGrounded || inWater) //and the player is grounded (or underwater for now)...
+			if (isGrounded) //and the player is grounded (or underwater for now)...
 			{
 				StartCoroutine(JumpWithDelay()); //start the JumpWithDelay coroutine
 			}
@@ -78,11 +81,15 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 
 		if (inWater)
 		{
-			rigidbodycomp.gravityScale = 1f;
+			rb.gravityScale = 0.3f;
+			moveSpeed = 6f;
+			jumpingPower = 8f;
 		}
 		else
 		{
-			rigidbodycomp.gravityScale = 2.5f;
+			rb.gravityScale = 2.5f;
+			moveSpeed = 8f;
+			jumpingPower = 16f;
 		}
 
 		bottomPoint = new Vector2(c2d.bounds.center.x, c2d.bounds.min.y); //the bottompoint variable equals the bottommost y point and center x point of the capsule collider
@@ -90,7 +97,7 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 		//Debug.DrawRay(transform.position, ToRotationVector2(AngleTo(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition))), Color.cyan); //gets the angle to the mouse position by using AngleTo from the player's position to the mouse position converted to a world point. The output is then converted to a Vector2 so a ray can be drawn at said angle
 
 		Debug.DrawRay(transform.position, DirectionTo(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition))); //better version
-		//Debug.Log("IsJumping: " + isJumping + " IsFalling: " + isFalling);
+																																 //Debug.Log("IsJumping: " + isJumping + " IsFalling: " + isFalling);
 
 		//Debug.Log(rb.velocity.y);
 
@@ -110,7 +117,11 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 	}
 	private void Movement()
 	{
-		float targetSpeed = xAxis * moveSpeed;
+		transform.rotation = Quaternion.Euler(0, 0, 0);
+
+		rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+		float targetSpeed = axis.x * moveSpeed;
 
 		float speedDif = targetSpeed - rb.velocity.x;
 
@@ -124,7 +135,7 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 
 		if (isGrounded) //if the player is grounded and isn't attacking
 		{
-			if (xAxis != 0) //if the player isn't still
+			if (axis.x != 0) //if the player isn't still
 			{
 				ChangeAnimationState(PlayerWalk); //set the animation to walking
 			}
@@ -133,12 +144,12 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 				ChangeAnimationState(PlayerIdle); //set the animation to idle
 			}
 		}
-		if (xAxis > 0)
+		if (axis.x > 0)
 		{
 			transform.localScale = new Vector2(1, 1); //facing right
 			isFacing = 1;
 		}
-		if (xAxis < 0)
+		if (axis.x < 0)
 		{
 			transform.localScale = new Vector2(-1, 1); //facing left
 			isFacing = -1;
@@ -148,7 +159,7 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 	{
 		isJumping = true; //set isjumping to true
 
-		rb.velocity = new Vector2 (rb.velocity.x, Vector2.zero.y);
+		rb.velocity = new Vector2(rb.velocity.x, Vector2.zero.y);
 
 		rb.AddForce(Vector2.up * jumpingPower, ForceMode2D.Impulse); //the jumping script
 	}
@@ -159,38 +170,90 @@ public class PlayerAI : NPC //basically, this script is a copy of the npc script
 			rb.AddForce(Vector2.down * rb.velocity.y * (1 - jumpReleaseMod), ForceMode2D.Impulse); //apply downward force to cut the player's jump
 		}
 	}
-	private void FixedUpdate() //for physics
+	private void Rise()
 	{
-		
-		Application.targetFrameRate = framerate; //target framerate equals the set number
-		#region GroundDetection
-		Color rayCol;
-		RaycastHit2D hit = Physics2D.Raycast(bottomPoint, Vector2.down, rayHeight, groundLayer);
+		rb.constraints = RigidbodyConstraints2D.None;
 
-		if (hit)
+		float targetSpeedy = axis.y * jumpingPower;
+
+		float speedDify = targetSpeedy - rb.velocity.y;
+
+		float accelRatey = (Mathf.Abs(targetSpeedy) > 0.01f) ? acceleration : deacceleration;
+
+		float movementy = Mathf.Pow(Mathf.Abs(speedDify) * accelRatey, velPower) * Mathf.Sign(speedDify);
+
+		rb.AddForce(movementy * Vector2.up);
+
+		float targetSpeedx = axis.x * moveSpeed;
+
+		float speedDifx = targetSpeedx - rb.velocity.x;
+
+		float accelRatex = (Mathf.Abs(targetSpeedx) > 0.01f) ? acceleration : deacceleration;
+
+		float movementx = Mathf.Pow(Mathf.Abs(speedDifx) * accelRatex, velPower) * Mathf.Sign(speedDifx);
+
+		rb.AddForce(movementx * Vector2.right);
+
+		if (axis.x != 0) //if the player isn't still
 		{
-			if (!isJumping)
-			{
-				float rayDirVel = Vector2.Dot(Vector2.down, rb.velocity); //math stuff
-				float otherDirVel = Vector2.Dot(Vector2.down, Vector2.zero);
-				float relVel = rayDirVel - otherDirVel;
-				float x = hit.distance - rideHeight;
-				float force = (x * rideSpringStrength) - (relVel * rideSpringDamper);
-
-				rb.AddForce(Vector2.down * force);
-			}
-
-			rayCol = Color.green;
-			isGrounded = true;
+			ChangeAnimationState(PlayerWalk); //set the animation to walking
 		}
 		else
 		{
-			rayCol = Color.red;
-			isGrounded = false;
+			ChangeAnimationState(PlayerIdle); //set the animation to idle
+		}
+
+		if (axis.x > 0)
+		{
+			transform.localScale = new Vector2(1, 1); //facing right
+			isFacing = 1;
+		}
+		if (axis.x < 0)
+		{
+			transform.localScale = new Vector2(-1, 1); //facing left
+			isFacing = -1;
+		}
+
+		//rb.AddForce(Vector2.up * jumpingPower, ForceMode2D.Impulse);
+	}
+	private void FixedUpdate() //for physics
+	{
+
+		Application.targetFrameRate = framerate; //target framerate equals the set number
+		#region GroundDetection
+
+		if (!inWater)
+		{
+			Color rayCol;
+			RaycastHit2D hit = Physics2D.Raycast(bottomPoint, transform.TransformDirection(Vector2.down), rayHeight, groundLayer);
+
+			if (hit)
+			{
+				if (!isJumping)
+				{
+					float rayDirVel = Vector2.Dot(transform.TransformDirection(Vector2.down), rb.velocity); //math stuff
+					float otherDirVel = Vector2.Dot(transform.TransformDirection(Vector2.down), Vector2.zero);
+					float relVel = rayDirVel - otherDirVel;
+					float x = hit.distance - rideHeight;
+					float force = (x * rideSpringStrength) - (relVel * rideSpringDamper);
+
+					rb.AddForce(Vector2.down * force);
+				}
+
+				rayCol = Color.green;
+				isGrounded = true;
+			}
+			else
+			{
+				rayCol = Color.red;
+				isGrounded = false;
+			}
 		}
 		#endregion
-
-		Movement();
+		if (!inWater)
+			Movement();
+		else
+			Rise();
 	}
 	private IEnumerator JumpWithDelay() //this entire thing just triggers jump and waits until the player is falling to change the variable
 	{
