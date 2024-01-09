5ad4522e49624afc6bac9d9abcdc9a99dce70193
Guard Boi <105892653+mariohero24@users.noreply.github.com>
2024-01-08 16:48:57 -0800

    Changes to the script to allow manual spawning later

--- Reign of Yharim/Assets/Scripts/WorldManagment/EnemySpawner.cs
+++ Reign of Yharim/Assets/Scripts/WorldManagment/EnemySpawner.cs
@@ -18,38 +18,42 @@ public class EnemySpawner : MonoBehaviour
     public float EnemySpawnRate;
     IEnumerator Start()
     {
-        int ground = 1 << LayerMask.NameToLayer("Ground");
         while(true)
         {
+            yield return SpawnEnemy(enemyToSpawn);
             yield return new WaitForSeconds(EnemySpawnRate);
-
-            Vector2 SpawnPosition = new Vector2(Random.Range(player.transform.position.x - 24f, player.transform.position.x + 24f), Random.Range(player.transform.position.y - 12f, player.transform.position.y + 12f));          
-
-            SpawnPosition = Physics2D.Raycast(SpawnPosition, Vector2.down, Mathf.Infinity, ground).point;
-            
-            //I can't make enemies not to spawn in blocks in general, but this will reduce chances of this happening by a lot
-            while (Physics2D.Raycast(SpawnPosition, Vector2.down, 0, ground))
-            {
-                for(int i = 0; i<10; i++)//The game will not stop working when there is no space to spawn an enemy
-                {
-                    if(Physics2D.Raycast(SpawnPosition, Vector2.down, 0, ground))
-                    {
-                        SpawnPosition = new Vector2(Random.Range(player.transform.position.x - 24f, player.transform.position.x + 24f), Random.Range(player.transform.position.y - 12f, player.transform.position.y + 12f));
-                        SpawnPosition = Physics2D.Raycast(SpawnPosition, Vector2.down, Mathf.Infinity, ground).point;
-                    }
-                    else break;
-                }
-
-                yield return new WaitForFixedUpdate();
-            }
-
-            GameObject newEnemy = Instantiate(enemyToSpawn, SpawnPosition, Quaternion.identity);
-            // newEnemy.GetComponent("Script").healthBar = 
-            if (newEnemy.scene != this.gameObject.scene)
-            {
-                SceneManager.MoveGameObjectToScene(newEnemy, this.gameObject.scene);
-            }
-            newEnemy.SetActive(true);
         }
+    }
+
+    IEnumerator SpawnEnemy(GameObject enemyPrefab)
+    {
+        int ground = 1 << LayerMask.NameToLayer("Ground");
+
+        Vector2 SpawnPosition = new Vector2(Random.Range(player.transform.position.x - 24f, player.transform.position.x + 24f), Random.Range(player.transform.position.y - 12f, player.transform.position.y + 12f));          
+
+        SpawnPosition = Physics2D.Raycast(SpawnPosition, Vector2.down, Mathf.Infinity, ground).point;
+        
+        //I can't make enemies not to spawn in blocks in general, but this will reduce chances of this happening by a lot
+        while (Physics2D.Raycast(SpawnPosition, Vector2.down, 0, ground))
+        {
+            for(int i = 0; i<10; i++)//The game will not stop working when there is no space to spawn an enemy
+            {
+                if(Physics2D.Raycast(SpawnPosition, Vector2.down, 0, ground))
+                {
+                    SpawnPosition = new Vector2(Random.Range(player.transform.position.x - 24f, player.transform.position.x + 24f), Random.Range(player.transform.position.y - 12f, player.transform.position.y + 12f));
+                    SpawnPosition = Physics2D.Raycast(SpawnPosition, Vector2.down, Mathf.Infinity, ground).point;
+                }
+                else break;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        GameObject newEnemy = Instantiate(enemyToSpawn, SpawnPosition, Quaternion.identity);
+        if (newEnemy.scene != this.gameObject.scene)
+        {
+            SceneManager.MoveGameObjectToScene(newEnemy, this.gameObject.scene);
+        }
+        newEnemy.SetActive(true);
     }
 }
