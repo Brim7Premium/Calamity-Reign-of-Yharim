cea2c469ece2a98d0946c357c708f982171befdb
Guard Boi <105892653+mariohero24@users.noreply.github.com>
2024-01-31 17:42:56 -0800

    dialogue queue

--- Reign of Yharim/Assets/Scripts/AI/NPC AI/DummyAI.cs
+++ Reign of Yharim/Assets/Scripts/AI/NPC AI/DummyAI.cs
@@ -1,33 +1,34 @@
 using UnityEngine;
 public class DummyAI : NPC
 {
-    public DialogueStuff dialogueStuff;
-    public override void SetDefaults()
-    {
-        base.SetDefaults();
+	public DialogueStuff dialogueStuff;
+	public override void SetDefaults()
+	{
+		base.SetDefaults();
 
-        Damage = 10;
-        NPCName = "Dummy";
-        LifeMax = 100;
-        Life = LifeMax;
-    }
-    public override void AI()
-    {
-        int randomNumber = Random.Range(1, 100);
-        Debug.Log(randomNumber);
-        UpdateVelocity();
-        if (target != null)
-        {
-            velocity = new Vector2(DirectionTo(transform.position, target.transform.position).x * 0.05f, velocity.y);
+		Damage = 10;
+		NPCName = "Dummy";
+		LifeMax = 100;
+		Life = LifeMax;
+	}
+	public override void AI()
+	{
+		int randomNumber = Random.Range(1, 100);
+		Debug.Log(randomNumber);
+		UpdateVelocity();
+		if (target != null)
+		{
+			velocity = new Vector2(DirectionTo(transform.position, target.transform.position).x * 0.05f, velocity.y);
 
-            if (1 == TargetDirection) //for looking at player
-                gameObject.transform.localScale = new Vector3(1, 1, 1);
-            else
-                gameObject.transform.localScale = new Vector3(-1, 1, 1);
-        }
-        if (randomNumber == 50)
-        {
-            dialogueStuff.SetDialogue("Keep yourself safe tonight Jacob Bryson", gameObject.GetComponent<SpriteRenderer>(), 2f); // holy shit its real
-        }
-    }
+			if (1 == TargetDirection) //for looking at player
+				gameObject.transform.localScale = new Vector3(1, 1, 1);
+			else
+				gameObject.transform.localScale = new Vector3(-1, 1, 1);
+		}
+		if (randomNumber == 50)
+		{
+			DialogueData dialogue = new DialogueData { text = "Keep yourself safe tonight Jacob Bryson", talkerSprite = gameObject.GetComponent<SpriteRenderer>().sprite, duration = 2f };
+			dialogueStuff.AddQueue(dialogue); // holy shit its real
+		}
+	}
 }
--- Reign of Yharim/Assets/Scripts/Player/GUI/DialogueStuff.cs
+++ Reign of Yharim/Assets/Scripts/Player/GUI/DialogueStuff.cs
@@ -4,29 +4,63 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+[System.Serializable] 
+public class DialogueData
+{
+	public string text;
+	public Sprite talkerSprite;
+	public float duration = 1f;
+}
+
 public class DialogueStuff : MonoBehaviour
 {
-    public Image talker;
-    public TMP_Text dialogue;
-    private GameObject dialogueBox;
+	public Image talker;
+	public TMP_Text dialogue;
+	private GameObject dialogueBox;
+	public bool isTalking;
+	public List<DialogueData> queue = new List<DialogueData>();
 
-    private void Start()
-    {
-        dialogueBox = gameObject;
-        dialogueBox.SetActive(false);
-    }
-    public void SetDialogue(string text, SpriteRenderer talkerSprite, float duration)
-    {
-        dialogueBox.SetActive(true);
-        dialogue.text = text;
-        talker.sprite = talkerSprite.sprite;
-        talker.color = talkerSprite.color;
-        StartCoroutine(DialogueWait(duration));
-    }
-
-    public IEnumerator DialogueWait(float seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-        dialogueBox.SetActive(false);
-    }
+	private void Start()
+	{
+		dialogueBox = gameObject;
+		if (queue.Count == 0)
+		{
+			isTalking = false;
+			dialogueBox.SetActive(false);
+		}
+		else
+		{
+			SetDialogue(queue[0]);
+		}
+	}
+	public void SetDialogue(DialogueData dialogueData)
+	{
+		isTalking = true;
+		dialogueBox.SetActive(true);
+		dialogue.text = dialogueData.text;
+		talker.sprite = dialogueData.talkerSprite;
+		StartCoroutine(DialogueWait(dialogueData.duration));
+	}
+	public IEnumerator DialogueWait(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		queue.RemoveAt(0);
+		if (queue.Count == 0)
+		{
+			isTalking = false;
+			dialogueBox.SetActive(false);
+		}
+		else
+		{
+			SetDialogue(queue[0]);
+		}
+	}
+	public void AddQueue(DialogueData dialoguedata)
+	{
+		queue.Add(dialoguedata);
+		if (!isTalking)
+		{
+			SetDialogue(dialoguedata);
+		}
+	}
 }
